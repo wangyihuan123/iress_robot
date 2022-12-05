cdf38f711a61f08a531ef8351bedd449387b8ba7
Ben Wang <wangyihuan123@hotmail.com>
2022-12-06 11:26:21 +1300

    First test:  move

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@
 
 
 using namespace std;
-namespace toy_robot{
+namespace toy_robot {
     // todo:
 }
 
@@ -18,9 +18,9 @@ enum class FaceDirection {
     EAST,
     WEST = 3
 };
-const string  directions[] = {"NORTH", "SOUTH", "EAST", "WEST"};
+const string directions[] = {"NORTH", "SOUTH", "EAST", "WEST"};
 
-class Robot{
+class Robot {
     int m_x = {0};
     int m_y = {0};
     FaceDirection m_face{FaceDirection::NORTH};
@@ -37,27 +37,27 @@ public:
         m_x = x;
     }
 
-        // todo: use hash table  instead of the switch later !!!
+    // todo: use hash table  instead of the switch later !!!
     void move() {
         switch (m_face) {
             case FaceDirection::NORTH:
                 if (m_y + 1 <= 5) {
-                    m_y ++;
+                    m_y++;
                 }
                 break;
             case FaceDirection::SOUTH:
                 if (m_y - 1 > 0) {
-                    m_y --;
+                    m_y--;
                 }
                 break;
             case FaceDirection::EAST:
                 if (m_x + 1 <= 5) {
-                    m_x ++;
+                    m_x++;
                 }
                 break;
             case FaceDirection::WEST:
                 if (m_y - 1 <= 5) {
-                    m_y --;
+                    m_y--;
                 }
                 break;
             default:
@@ -69,26 +69,29 @@ public:
     }
 
     void report(void) {
-        assert(m_x<=5 && m_x >=0);
-        assert(m_y<=5 && m_y >=0);
-        assert(m_face<=FaceDirection::WEST && m_face >=FaceDirection::NORTH);
+        assert(m_x <= 5 && m_x >= 0);
+        assert(m_y <= 5 && m_y >= 0);
+        assert(m_face <= FaceDirection::WEST && m_face >= FaceDirection::NORTH);
 
         cout << "OUTPUT: " << m_x << "," << m_y << "," << directions[static_cast<int>(m_face)] << endl;
     }
 };
 
 
-void parseLineInput(unique_ptr<Robot> & robot, string strLine)
-{
-
+void test(void) {
+    Robot robot;
+    robot.place(1,1);
+    robot.report();
+    robot.move();
+    robot.report();
 }
 
-int main(int argc, char** argv) {
+void test_file(const string filename) {
     auto robot = make_unique<Robot>();
 
     string strLine;
     ifstream inFile;
-    inFile.open(argv[1]);
+    inFile.open(filename);
     if(inFile.is_open())
     {
         while(getline(inFile, strLine)) {
@@ -96,12 +99,18 @@ int main(int argc, char** argv) {
 //            parseLineInput(robot, strLine);
         }
         inFile.close();
-        return 0;
     }
     else
     {
-        cout<<"Cannot open file: "<<argv[1]<<endl;
+        cout<<"Cannot open file: "<< filename<<endl;
     }
+}
+
+
+int main(int argc, char **argv) {
+    test();
+
+//    test_file(argv[1]);
 
     return 0;
 }
