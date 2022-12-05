878a712378705f0930e156063ce4668b41f488f6
Ben Wang <wangyihuan123@hotmail.com>
2022-12-06 12:20:00 +1300

    test left/right

--- main.cpp
+++ main.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
 #include <string>
-#include <assert.h>
-
-#include <stdlib.h>
+#include <cassert>
+#include <cstdlib>
 #include <memory>
 #include <fstream>
 
@@ -12,18 +11,21 @@ namespace toy_robot {
     // todo:
 }
 
-enum class FaceDirection {
+enum class Direction {
     NORTH = 0,
-    SOUTH,
     EAST,
-    WEST = 3
+    SOUTH,
+    WEST = 3,
+    MinDirection = NORTH,
+    MaxDirection = WEST,
 };
-const string directions[] = {"NORTH", "SOUTH", "EAST", "WEST"};
+
+const string directions[] = {"NORTH", "EAST", "SOUTH", "WEST"};
 
 class Robot {
     int m_x = {0};
     int m_y = {0};
-    FaceDirection m_face{FaceDirection::NORTH};
+    Direction m_facing{Direction::NORTH};
 
 public:
 
@@ -39,54 +41,127 @@ public:
 
     // todo: use hash table  instead of the switch later !!!
     void move() {
-        switch (m_face) {
-            case FaceDirection::NORTH:
+        switch (m_facing) {
+            case Direction::NORTH:
                 if (m_y + 1 <= 5) {
                     m_y++;
                 }
                 break;
-            case FaceDirection::SOUTH:
+            case Direction::SOUTH:
                 if (m_y - 1 > 0) {
                     m_y--;
                 }
                 break;
-            case FaceDirection::EAST:
+            case Direction::EAST:
                 if (m_x + 1 <= 5) {
                     m_x++;
                 }
                 break;
-            case FaceDirection::WEST:
+            case Direction::WEST:
                 if (m_y - 1 <= 5) {
                     m_y--;
                 }
                 break;
             default:
                 // throw
-                cout << "FaceDirection:" << static_cast<int>(m_face) << endl;
+                cout << "Direction:" << static_cast<int>(m_facing) << endl;
                 abort();
 
         }
     }
 
-    void report(void) {
+
+    void right () {
+        // todo: check placed or not
+
+        // If performance matters, I can  use m_facing = (m_facing + 1) % 4;
+        // overload enum class may be needed.
+        // But now, use switch statement for readable code
+        switch (m_facing)
+        {
+            case Direction::NORTH:
+                m_facing = Direction::EAST;
+                break;
+            case Direction::EAST:
+                m_facing = Direction::SOUTH;
+                break;
+            case Direction::SOUTH:
+                m_facing = Direction::WEST;
+                break;
+            case Direction::WEST:
+                m_facing = Direction::NORTH;
+                break;
+            default:
+                // throw
+                cout << "Direction:" << static_cast<int>(m_facing) << endl;
+                abort();
+        }
+    }
+
+
+    void left( )
+    {
+        // todo: check placed or not
+
+        // If performance matters, I can  use m_facing = (m_facing + 3) % 4;
+        // overload enum class may be needed.
+        // But now, use switch statement for readable code
+        switch (m_facing)
+        {
+            case Direction::NORTH:
+                m_facing = Direction::WEST;
+                break;
+            case Direction::EAST:
+                m_facing = Direction::NORTH;
+                break;
+            case Direction::SOUTH:
+                m_facing = Direction::EAST;
+                break;
+            case Direction::WEST:
+                m_facing = Direction::SOUTH;
+                break;
+            default:
+                // throw
+                cout << "Direction:" << static_cast<int>(m_facing) << endl;
+                abort();
+        }
+    }
+
+    void report() {
         assert(m_x <= 5 && m_x >= 0);
         assert(m_y <= 5 && m_y >= 0);
-        assert(m_face <= FaceDirection::WEST && m_face >= FaceDirection::NORTH);
+        assert(m_facing <= Direction::WEST && m_facing >= Direction::NORTH);
 
-        cout << "OUTPUT: " << m_x << "," << m_y << "," << directions[static_cast<int>(m_face)] << endl;
+        cout << "OUTPUT: " << m_x << "," << m_y << "," << directions[static_cast<int>(m_facing)] << endl;
     }
 };
 
 
-void test(void) {
+void test() {
     Robot robot;
-    robot.place(1,1);
     robot.report();
     robot.move();
     robot.report();
+    robot.place(1,1);
+    robot.report();
+    robot.right();
+    robot.report();
+    robot.move();
+    robot.report();
+    robot.move();
+    robot.report();
+    robot.move();
+    robot.report();
+    robot.move();
+    robot.move();
+    robot.move();
+    robot.report();
+    robot.left();
+    robot.report();
+
 }
 
-void test_file(const string filename) {
+void test_file(const string & filename) {
     auto robot = make_unique<Robot>();
 
     string strLine;
